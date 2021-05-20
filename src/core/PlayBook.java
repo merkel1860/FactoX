31714126676d9038c02343011bbf07d19643fdd6
merkel1860 <merkelmller@yandex.ru>
2021-05-20 13:47:37 -0400

    Initial commit Factorial Function

---
+++ src/core/PlayBook.java
@@ -0,0 +1,37 @@
+package core;
+
+import java.util.Scanner;
+
+public class PlayBook {
+
+    public static void main(String[] args) {
+	// write your code here
+        int value;
+        int result;
+        value = readValue();
+        result = factoX(value);
+        display(value, result);
+    }
+
+    private static void display(int value, int result) {
+        System.out.println("Le facto de "+value+" est :"+result);
+    }
+
+    private static int factoX(int value) {
+        int temp = 1;
+        if(value == 1){
+            temp = 1;
+        }else if(value > 1){
+            temp = value * factoX(value-1);
+        }
+
+        return temp;
+    }
+
+    private static int readValue() {
+        System.out.println("Enter numeric value :");
+        Scanner scanner = new Scanner(System.in);
+        int v = Integer.valueOf(scanner.nextLine());
+        return v;
+    }
+}
